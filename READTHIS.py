133ef2656b6f43b206ef5cecc5727463b0655f4c
Jhazgorg <eschwander@csumb.edu>
2015-12-16 18:09:05 -0800

    Added a small detail to READTHIS.py

--- READTHIS.py
+++ READTHIS.py
@@ -1,6 +1,7 @@
 '''
 For this parser to run you need to have a file called tcrtIPs.py.
 This file holds the IP addresses of the California, Oregon, and East Coast servers.
+These IPs are sensitive info, hence the x's.
 CaliforniaIP = xxx.xxx.xxx.xxx
 OregonIP = xxx.xxx.xxx.xxx
 EastCoastIP = xxx.xxx.xxx.xxx
